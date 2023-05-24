a8a78bf3b8534c89870569777836b7fc59f367e1
virkann <virkan90@gmail.com>
2023-05-24 15:13:14 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+
+// 1- task
+
+// 2- task
+
+    // function nSidedShape(n) {
+    //     	const obj = {
+    //     		1: "circle",
+    //     		2: "semi-circle",
+    //     		3: "triangle",
+    //     		4: "square",
+    //     		5: "pentagon",
+    //     		6: "hexagon",
+    //     		7: "heptagon",
+    //     		8: "octagon",
+    //     		9: "nonagon",
+    //     		10: "decagon",
+    //     	}
+    //     	return obj[n]
+    //     }
+    //     console.log(nSidedShape(3));
+    //     console.log(nSidedShape(1));
+    //     console.log(nSidedShape(9));
+
+// 3- task
+
+// 4- task
+
+// 5- task
+
+
